6b75d3227e3a8e97409ee7d8009dc7a02c9c107e
Hadar Yanai <yanaih@post.bgu.ac.il>
2026-04-18 16:44:52 +0300

    HRRequirement menu

--- dev/domain/RequestStatus.java
+++ dev/domain/RequestStatus.java
@@ -1,7 +1,18 @@
 package dev.domain;
 
 public enum RequestStatus {
-    PENDING,
-    APPROVED,
-    REJECTED
+
+    PENDING("Pending"),
+    APPROVED("Approved"),
+    REJECTED("Rejected");
+
+    private final String value;
+
+    RequestStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
